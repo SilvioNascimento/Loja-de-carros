b952d81fba245ae1e7fc2d7d5be65614d1cb7503
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-09-08 15:32:09 -0300

    Adicionando dados sobre carros

---
+++ js/carros.js
@@ -0,0 +1,26 @@
+let carros = [
+    {
+        titulo: "Tesla Model 3",
+        imagem: "/img/Tesla_Model_3.jpg",
+        marca: "Tesla",
+        anoLancamento: 2017,
+        descricao: "O Model 3 é um sedan elétrico de luxo, conhecido por sua autonomia, aceleração e tecnologia de ponta. Possui um design minimalista e aerodinâmico, além de um interior futurista com tela touchscreen gigante.",
+        preco: "R$230.000,00"
+    },
+    {
+        titulo: "Fiat Cronos",
+        imagem: "/img/Fiat_Cronos.jpg",
+        marca: "Fiat",
+        anoLancamento: 2018,
+        descricao: "O Cronos é um sedan compacto da Fiat, conhecido por seu design moderno e motor eficiente. Oferece um bom espaço interno e diversas opções de personalização.",
+        preco: "R$75.000,00"
+    },
+    {
+        titulo: "Hyundai HB20",
+        imagem: "/img/Hyundai_HB20.jpg",
+        marca: "Hyundai",
+        anoLancamento: 2012,
+        descricao: "O HB20 é um hatchback compacto da Hyundai, conhecido por seu design jovem e esportivo. É um dos carros mais vendidos no Brasil e oferece um bom custo-benefício.",
+        preco: "R$60.000,00"
+    }
+];
